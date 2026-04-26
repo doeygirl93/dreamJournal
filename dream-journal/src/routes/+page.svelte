af4505899257ff7faa1a85197266cf9e97ac48ad
Avishi Sharma <robloxpusheen8@gmail.com>
2026-04-26 03:18:11 -0400

    work on main page

--- dream-journal/src/routes/+page.svelte
+++ dream-journal/src/routes/+page.svelte
@@ -45,7 +45,7 @@
 <div class="body">
     <div class="signlog">
             <div>
-                <h1>Log In</h1>
+                <h1 class="loginbig">Log In</h1>
                 <p>Username</p>
                 <input type="text" placeholder="username" bind:value={loginUsername}>
                 <p>Password</p>
@@ -67,84 +67,123 @@
 </div>
 
 <style>
-    .body{
-        margin:0%;
-        font-family:sans-serif;
-    }
+body{
+    margin:0%;
+    font-family:sans-serif;
+}
 
-    .signlog{
-        display:flex;
-        justify-content: center;
-        height:100vh;
-        width:100vw;
-        gap:3%;
-        flex-direction: column;
-        align-items: center;
-        background: #733535;
-        background: linear-gradient(360deg, rgba(115, 53, 53, 1) 0%, rgba(94, 45, 117, 1) 38%, rgba(14, 38, 99, 1) 100%);
+.signlog{
+    display:flex;
+    justify-content: center;
+    height:100vh;
+    width:100vw;
+    gap:3%;
+    flex-direction: column;
+    align-items: center;
+    background: #733535;
+    background: linear-gradient(360deg, rgba(115, 53, 53, 1) 0%, rgba(94, 45, 117, 1) 38%, rgba(14, 38, 99, 1) 100%);
 
-    }
+}
 
-    .signlog > *{
-        padding:10%;
-        border-color:rgb(244, 244, 244);;
-        border-radius:20%;
-        border-style:solid;
-        border-width:5px;
-        width:fit-content;
-        height:fit-content;
-        background-color:#d8fffe95;
-        opacity:0.4;
-        transition:transform 10s ease-in-out;
-    }
+.signlog > *{
+    padding:7%;
+    border-color:rgb(244, 244, 244);;
+    border-radius:20%;
+    border-style:solid;
+    border-width:5px;
+    width:fit-content;
+    height:fit-content;
+    background-color:#d8fffe95;
+    opacity:0.4;
+    transition:transform 500ms ease-in-out;
+}
 
-    .signlog >* >*:hover{
-        transform:scale(1.05);
-    }
+/*.signlog >* >*:hover{
+    transform:scale(1.05);
 
-    button{
-        text-decoration:none;
-        border:none;
-        background:none;
-        font-size:45px;
-        opacity:1;
-    }
+}
+    */
 
-    .star1{
-        width:2px;
-        height:2px;
-        border-radius:100%;
-        animation: twinkle ease-in-out infinite;
-        box-shadow: 0px 0px 6px 1px;
-    }
+.signlog>*>*>*:hover{
+    transform:scale(1.05);
 
-    .star2{
-        width:4px;
-        height:4px;
-        border-radius:100%;
-        box-shadow: 0px 0px 6px 1px;
-        animation: twinkle ease-in-out infinite;
+}
 
-    }
+.signlog >*>*{
+    display:flex;
+    flex-direction: column;
+    row-gap: 10px;
 
-    .star3{
-        width:6px;
-        height:6px;
-        border-radius:100%;
-        box-shadow: 0px 0px 6px 1px;
-        animation: twinkle ease-in-out infinite;
+}
 
-    }
+.signlog >* h1{
+    font-size:60px;
 
-    @keyframes twinkle{
-        50%{opacity:0;}
-    }
+}
 
-    input{
-        background-color:rgba(255, 255, 255, 0.523);
-        border-radius:10px;
-        margin-left:10px;
-        padding:5px;
+.login{
+    margin:5% 0% 5% 0%;
+    border-radius:10px;
+    box-shadow: 0px 0px 6px 1px;
+    background-color:white;
 
-    }
+}
+
+.loggin{
+    margin-top:15%;
+    
+
+}
+
+button{
+    text-decoration:none;
+    border:none;
+    background:none;
+    font-size:45px;
+    opacity:1;
+}
+
+.star1{
+    width:2px;
+    height:2px;
+    border-radius:100%;
+    animation: twinkle ease-in-out infinite;
+    box-shadow: 0px 0px 6px 1px;
+}
+
+.star2{
+    width:4px;
+    height:4px;
+    border-radius:100%;
+    box-shadow: 0px 0px 6px 1px;
+    animation: twinkle ease-in-out infinite;
+
+}
+
+.star3{
+    width:6px;
+    height:6px;
+    border-radius:100%;
+    box-shadow: 0px 0px 6px 1px;
+    animation: twinkle ease-in-out infinite;
+
+}
+
+@keyframes twinkle{
+    50%{opacity:0;}
+}
+
+input{
+    background-color:rgba(255, 255, 255, 0.523);
+    border-radius:10px;
+    margin-left:10px;
+    padding:5px;
+
+}
+
+.loginbig{
+    color:blue;
+    
+}
+
 </style>
